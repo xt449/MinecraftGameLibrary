2f9afa6d91bfe054c370a6e28e6127fa21e5bc93
xt449 <alpha.xt449.omega@gmail.com>
2020-09-14 18:10:55 -0400

    Version 1.0.1-Beta Updated to Minecraft 1.16.3 Added missing license notice to files

---
+++ src/main/java/com/github/xt449/minecraftgamelibrary/ManaSystem.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2020 xt449/BinaryBanana
+ *
+ * This file is part of MinecraftGameLibrary.
+ *
+ * MinecraftGameLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MinecraftGameLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MinecraftGameLibrary.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.github.xt449.minecraftgamelibrary;
+
+import org.bukkit.entity.Player;
+
+public class ManaSystem {
+
+	final Checker checker;
+	final Spender spender;
+
+	final ManaFailure manaFailure;
+	final CooldownFailure cooldownFailure;
+
+	public ManaSystem(Checker checker, Spender spender, ManaFailure manaFailure, CooldownFailure cooldownFailure) {
+		this.checker = checker;
+		this.spender = spender;
+
+		this.manaFailure = manaFailure;
+		this.cooldownFailure = cooldownFailure;
+	}
+
+	@FunctionalInterface
+	public static interface Checker {
+		boolean check(Spell spell, Player player, int mana);
+	}
+
+	@FunctionalInterface
+	public static interface Spender {
+		void spend(Spell spell, Player player, int mana);
+	}
+
+	@FunctionalInterface
+	public static interface ManaFailure {
+		void execute(Spell spell, Player player);
+	}
+
+	@FunctionalInterface
+	public static interface CooldownFailure {
+		void execute(Spell spell, Player player);
+	}
+}
--- src/main/java/com/github/xt449/minecraftgamelibrary/Spell.java
+++ src/main/java/com/github/xt449/minecraftgamelibrary/Spell.java
@@ -1,17 +1,40 @@
+/*
+ * Copyright (c) 2020 xt449/BinaryBanana
+ *
+ * This file is part of MinecraftGameLibrary.
+ *
+ * MinecraftGameLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MinecraftGameLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MinecraftGameLibrary.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.github.xt449.minecraftgamelibrary;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.Objects;
 
 public class Spell {
 
@@ -21,156 +44,180 @@ public class Spell {
 	public final Material material;
 	public final int mana;
 	public final int cooldown;
-	private final Function<Player, Boolean> action;
-	private final Consumer<Player> onFailCooldown;
-	private final Consumer<Player> onFailMana;
+	protected final Action action;
+	protected final ChatColor chatColor;
 
 	/**
-	 * @param mana     cost in experience levels
-	 * @param cooldown in ticks
+	 * @param name      name for itemstack
+	 * @param material  material for itemstack
+	 * @param mana      mana cost
+	 * @param cooldown  cooldown in ticks
+	 * @param action    action on cast
 	 */
-	public Spell(@NotNull String name, @NotNull Material material, int mana, int cooldown, @NotNull Function<Player, Boolean> action, @NotNull Consumer<Player> onFailCooldown, @NotNull Consumer<Player> onFailMana) {
+	public Spell(@NotNull String name, @NotNull Material material, int mana, int cooldown, @NotNull Action action) {
 		this.name = name;
 		this.material = material;
 		this.mana = mana;
 		this.cooldown = cooldown;
 		this.action = action;
-		this.onFailCooldown = onFailCooldown;
-		this.onFailMana = onFailMana;
+		this.chatColor = ChatColor.WHITE;
 	}
 
 	/**
-	 * @param mana     cost in experience levels
-	 * @param cooldown in ticks
+	 * @param name      name for itemstack
+	 * @param material  material for itemstack
+	 * @param mana      mana cost
+	 * @param cooldown  cooldown in ticks
+	 * @param action    action on cast
+	 * @param chatColor color for itemstack name
 	 */
-	public Spell(@NotNull String name, @NotNull Material material, int mana, int cooldown, @NotNull Function<Player, Boolean> action) {
+	public Spell(@NotNull String name, @NotNull Material material, int mana, int cooldown, @NotNull Action action, @NotNull ChatColor chatColor) {
 		this.name = name;
 		this.material = material;
 		this.mana = mana;
 		this.cooldown = cooldown;
 		this.action = action;
-		this.onFailCooldown = Spell::emptyPlayerConsumer;
-		this.onFailMana = Spell::emptyPlayerConsumer;
+		this.chatColor = chatColor;
 	}
 
-//	/**
-//	 * @param mana     cost in experience levels
-//	 * @param cooldown in ticks
-//	 */
-//	public Spell(String name, Material material, int mana, int cooldown, Function<Player, Boolean> action) {
-//		this.name = name;
-//		this.material = material;
-//		this.mana = mana;
-//		this.cooldown = cooldown;
-//		this.action = action;
-//	}
-//
-//	/**
-//	 * @param mana     cost in experience levels
-//	 * @param cooldown in ticks
-//	 */
-//	public Spell(String name, Material material, int mana, int cooldown, Function<Player, Boolean> action) {
-//		this.name = name;
-//		this.material = material;
-//		this.mana = mana;
-//		this.cooldown = cooldown;
-//		this.action = action;
-//	}
+	protected void startCooldown(@NotNull Player player) {
+		this.playerCooldowns.put(player, this.cooldown);
+	}
+
+	@Deprecated
+	protected void tickCooldowns() {
+		for(HashMap.Entry<Player, Integer> kvp : this.playerCooldowns.entrySet()) {
+			if(kvp.getValue() != null && kvp.getValue() > 0) {
+				this.playerCooldowns.put(kvp.getKey(), kvp.getValue() - 1);
+			}
+		}
+	}
+
+	@Deprecated
+	protected void tickCooldown(@NotNull Player player) {
+		final Integer remainingCooldown = this.playerCooldowns.get(player);
+		if(remainingCooldown != null && remainingCooldown > 0) {
+			this.playerCooldowns.put(player, remainingCooldown - 1);
+		}
+	}
 
 	public void cast(@NotNull Player player) {
 		if(!onCooldown(player)) {
-			if(manaChecker(player, this.mana)) {
+			if(manaSystem.checker.check(this, player, this.mana)) {
 				if(this.action.apply(player)) {
 					startCooldown(player);
-					manaSpender(player, this.mana);
+					manaSystem.spender.spend(this, player, this.mana);
 				}
 			} else {
-				onFailMana.accept(player);
+				manaSystem.manaFailure.execute(this, player);
 				final TextComponent text = new TextComponent("Not enough mana");
 				text.setColor(ChatColor.DARK_AQUA);
 				player.spigot().sendMessage(ChatMessageType.ACTION_BAR, text);
 			}
 		} else {
-			onFailCooldown.accept(player);
+			manaSystem.cooldownFailure.execute(this, player);
 			final TextComponent text = new TextComponent("Cooldown has " + decimalFormat.format(getRemainingCooldown(player) / 20F) + " seconds remaining");
 			text.setColor(ChatColor.DARK_AQUA);
 			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, text);
 		}
 	}
 
+	/**
+	 * Register this spell for cooldown ticking via {@link Spell#tickAllCooldowns()}
+	 */
 	public final void register() {
 		Spell.registered.add(this);
 	}
 
-	public final void registerInteractiveLeftClick() {
-		Spell.registered.add(this);
-		Spell.registeredInteractiveLeftClick.put(this.material, this);
-	}
-
-	public final void registerInteractiveRightClick() {
-		Spell.registered.add(this);
-		Spell.registeredInteractiveRightClick.put(this.material, this);
-	}
-
-	public int getRemainingCooldown(Player player) {
+	/**
+	 * @param player caster
+	 * @return remaining cooldown in ticks for given player
+	 */
+	public int getRemainingCooldown(@NotNull Player player) {
 		final Integer remainingCooldown = this.playerCooldowns.get(player);
 		return remainingCooldown != null ? remainingCooldown : 0;
 	}
 
-	public boolean onCooldown(Player player) {
+	/**
+	 * @param player caster
+	 * @return true if cooldown is active for given player
+	 */
+	public boolean onCooldown(@NotNull Player player) {
 		final Integer remainingCooldown = this.playerCooldowns.get(player);
 		return remainingCooldown != null && remainingCooldown > 0;
 	}
 
-	private void startCooldown(Player player) {
-		this.playerCooldowns.put(player, this.cooldown);
+	public ItemStack getItemStack() {
+		final ItemStack itemStack = new ItemStack(material);
+		final ItemMeta itemMeta = itemStack.getItemMeta();
+		itemMeta.setDisplayName(chatColor + name);
+		itemMeta.setLore(Arrays.asList(org.bukkit.ChatColor.DARK_AQUA + "Mana: " + mana, org.bukkit.ChatColor.RED + "Cooldown: " + (cooldown / 20F) + " seconds"));
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
 	}
 
-//	private void tickCooldowns() {
-//		for(Map.Entry<Player, Integer> kvp : this.playerCooldowns.entrySet()) {
-//			if(kvp.getValue() != null && kvp.getValue() > 0) {
-//				this.playerCooldowns.put(kvp.getKey(), kvp.getValue() - 1);
-//			}
-//		}
-//		/*for(Player player : this.playerCooldowns.keySet()) {
-//			tickCooldown(player);
-//		}*/
-//	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
 
-//	private void tickCooldown(Player player) {
-//		final Integer remainingCooldown = this.playerCooldowns.get(player);
-//		if(remainingCooldown != null && remainingCooldown > 0) {
-//			this.playerCooldowns.put(player, remainingCooldown - 1);
-//		}
-//		/*if(remainingCooldown != null) {
-//			if(remainingCooldown > 0) {
-//				this.playerCooldowns.put(player, remainingCooldown - 1);
-//			}
-//		} else {
-//			this.playerCooldowns.put(player, 0);
-//		}*/
-//	}
+	@Override
+	public boolean equals(Object other) {
+		if(this == other) return true;
+		if(other == null || getClass() != other.getClass()) return false;
+		final Spell spell = (Spell) other;
+		return Objects.equals(name, spell.name) &&
+				mana == spell.mana &&
+				material == spell.material &&
+				cooldown == spell.cooldown &&
+				Objects.equals(action, spell.action);
+	}
 
 	// static
 
-	private static final DecimalFormat decimalFormat = new DecimalFormat("#.##");
-
 	private static final HashSet<Spell> registered = new HashSet<>();
-	private static final HashMap<Material, Spell> registeredInteractiveLeftClick = new HashMap<>();
-	private static final HashMap<Material, Spell> registeredInteractiveRightClick = new HashMap<>();
+
+	private static ManaSystem manaSystem = new ManaSystem(
+			(spell, player, mana) -> mana <= player.getLevel(),
+			(spell, player, mana) -> {
+				final int remaining = player.getLevel() - mana;
+				if(remaining < 0) {
+					player.setLevel(0);
+				} else {
+					player.setLevel(remaining);
+				}
+			},
+			(spell, player) -> {
+				player.playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 0.2F, 1);
+				final TextComponent text = new TextComponent("Requires " + spell.mana + " mana");
+				text.setColor(ChatColor.DARK_AQUA);
+				player.spigot().sendMessage(ChatMessageType.ACTION_BAR, text);
+			},
+			(spell, player) -> {
+				player.playSound(player.getLocation(), Sound.ENTITY_CREEPER_PRIMED, 0.2F, 1);
+				final TextComponent text = new TextComponent("Cooldown has " + Spell.decimalFormat.format(spell.getRemainingCooldown(player) / 20F) + " seconds remaining");
+				text.setColor(ChatColor.DARK_AQUA);
+				player.spigot().sendMessage(ChatMessageType.ACTION_BAR, text);
+			}
+	);
+
+	protected static DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
 	public static HashSet<Spell> getAll() {
 		return Spell.registered;
 	}
 
-	public static Spell getByInteractiveLeftClickMaterial(Material material) {
-		return Spell.registeredInteractiveLeftClick.get(material);
+	public static void setManaSystem(ManaSystem manaSystem) {
+		Spell.manaSystem = manaSystem;
 	}
 
-	public static Spell getByInteractiveRightClickMaterial(Material material) {
-		return Spell.registeredInteractiveRightClick.get(material);
+	public static void setDecimalFormat(DecimalFormat decimalFormat) {
+		Spell.decimalFormat = decimalFormat;
 	}
 
+	/**
+	 * Tick all registered spells' cooldowns
+	 */
 	public static void tickAllCooldowns() {
 		for(Spell spell : Spell.registered) {
 			for(HashMap.Entry<Player, Integer> kvp : spell.playerCooldowns.entrySet()) {
@@ -182,20 +229,12 @@ public class Spell {
 		}
 	}
 
-	private static void emptyPlayerConsumer(Player player) {
-
-	}
-
-	private static boolean manaChecker(Player player, int mana) {
-		return mana <= player.getLevel();
-	}
-
-	private static void manaSpender(Player player, int mana) {
-		final int remaining = player.getLevel() - mana;
-		if(remaining < 0) {
-			player.setLevel(0);
-		} else {
-			player.setLevel(remaining);
-		}
+	@FunctionalInterface
+	public static interface Action {
+		/**
+		 * @param player spell caster of action
+		 * @return true if spell action casts successfully
+		 */
+		boolean apply(Player player);
 	}
 }
